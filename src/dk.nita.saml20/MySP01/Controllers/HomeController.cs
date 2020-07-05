f303b7e6f1619cff329bb36b5c8927cbdf27172b
Torben Kjeldsen <torben@kjeldsendomain.dk>
2020-07-05 22:08:27 +0200

    initial commit

---
+++ src/dk.nita.saml20/MySP01/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MySP01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.IsAuthenticated = User.Identity.IsAuthenticated;
+            return View();
+        }
+
+
+        public ActionResult AuthenticatedOnly()
+        {
+            ViewBag.IsAuthenticated = User.Identity.IsAuthenticated;
+            ViewBag.Message = "Hvis du når hertil er du blevet authenticated";
+
+            return View();
+        }
+
+        ///
+        public ActionResult Login()
+        {
+            return Redirect("/login.ashx?ReturnUrl=/");
+        }
+
+        public ActionResult Logout()
+        {
+            return Redirect("/logout.ashx");
+        }
+
+    }
+}
---
+++ src/dk.nita.saml20/MySP01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MySP01.MvcApplication" Language="C#" %>
