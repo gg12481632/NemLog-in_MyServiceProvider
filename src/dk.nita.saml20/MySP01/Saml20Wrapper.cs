3a83c7f25a6b602f7350bc6172696863f2fd0a91
Torben Kjeldsen <gg12481632@gmail.com>
2020-07-08 16:35:53 +0200

    x

---
+++ src/dk.nita.saml20/MySP01/Saml20Wrapper.cs
@@ -0,0 +1,31 @@
+﻿using dk.nita.saml20.protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.SessionState;
+
+namespace Saml20Wrapper
+{
+    public class Saml20SignonHandlerWrapper : IHttpHandler, IRequiresSessionState
+    {
+        private Saml20SignonHandler _handler;
+
+        public Saml20SignonHandlerWrapper()
+        {
+            _handler = new Saml20SignonHandler();
+        }
+
+        public int MyProperty { get; set; }
+
+
+        public bool IsReusable { 
+            get { return _handler.IsReusable; } 
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            _handler.ProcessRequest(context);
+        }
+    }
+}
