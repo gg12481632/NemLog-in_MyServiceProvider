b41710b1b69aca938ff2388d0bde00ca4065e5b3
Torben Kjeldsen <torben@kjeldsendomain.dk>
2020-07-29 07:09:31 +0200

    .

--- myinstall/idp_setup_anysoft_dk/step1_create_idp_signing_certificate.ps1
+++ myinstall/idp_setup_anysoft_dk/step1_create_idp_signing_certificate.ps1
@@ -11,14 +11,14 @@ set-location $PSScriptRoot
 $certpassword = ConvertTo-SecureString -String "test1234" -AsPlainText -Force
 
 #Create a Selfsigned signing certificate for the IDP available at: idp.asnysoft.dk
-$cert = New-SelfSignedCertificate -DnsName idp.anysoft.dk2 -KeyFriendlyName IdpSigningCertificate  -CertStoreLocation cert:\LocalMachine\My
+$cert = New-SelfSignedCertificate -DnsName idp.anysoft.dk -KeyFriendlyName IdpSigningCertificate  -CertStoreLocation cert:\LocalMachine\My
 $pwd = ConvertTo-SecureString -String "test1234" -Force -AsPlainText
-Export-PfxCertificate -Cert $cert -FilePath '..\certificates\selfsigned_signing_certificate_idp_anysoft_dk2.pfx' -Password $pwd
+Export-PfxCertificate -Cert $cert -FilePath '..\certificates\ssc_idp_anysoft_dk.pfx' -Password $pwd
 write-host "Installed identintity provider signing certificate $($sslcertificate.Thumbprint) in LocalMachine\My and LocalMachine\TrustedPeople. This ensures the certificate is trusted on your machine and browser"
 
 write-host "Installing identity provider signing certificate"
-$serviceprovidercertificate = Import-PfxCertificate '..\certificates\selfsigned_signing_certificate_idp_anysoft_dk2.pfx' -Password $certpassword -CertStoreLocation Cert:\LocalMachine\My
-$serviceprovidercertificate = Import-PfxCertificate '..\certificates\selfsigned_signing_certificate_idp_anysoft_dk2.pfx' -Password $certpassword -CertStoreLocation Cert:\LocalMachine\TrustedPeople
+$serviceprovidercertificate = Import-PfxCertificate '..\certificates\ssc_idp_anysoft_dk.pfx' -Password $certpassword -CertStoreLocation Cert:\LocalMachine\My
+$serviceprovidercertificate = Import-PfxCertificate '..\certificates\ssc_idp_anysoft_dk.pfx' -Password $certpassword -CertStoreLocation Cert:\LocalMachine\TrustedPeople
 
 write-host "Setup completed!"
 
